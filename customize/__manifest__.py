80331696c4b825df1fc6eebcea5aca9e92b35c37
Heifara MATAPO <heifara@digitaltechno.net>
2024-01-25 13:22:21 -1000

    [ADD] customize: Initial commit

---
+++ customize/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Personnalisation",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "EPIC Te Ito Rau no Moorea Maiao",
+    'website': "http://www.yourcompany.com",
+    'category': 'Studio',
+    'version': '0.0.1',
+    'depends': [],
+    'data': [
+    ],
+}
